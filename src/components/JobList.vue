bf54cbf5a3f944d264aa405161d6f84faf6f9b79
Adolfo <adolfo.guevara.11@gmail.com>
2023-02-15 18:09:08 -0300

    buttons and types to order

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="job-list">
+    <p>
+      Order By: {{ order }}
+    </p>
     <ul>
       <li v-for="job in jobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
@@ -20,12 +23,17 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import Job from '@/types/Job';
+import OrderTerms from '@/types/OrderTerm';
 
 export default defineComponent({
   props: {
     jobs: {
       required: true,
       type: Array as PropType<Job[]>
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerms>
     }
   }
 })
